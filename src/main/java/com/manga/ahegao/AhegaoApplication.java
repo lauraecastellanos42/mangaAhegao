cd8ee62b5a28cf32c9d245b73e4a73e6f4634a24
lauraecastellanos42 <100247144+lauraecastellanos42@users.noreply.github.com>
2022-02-24 20:39:02 -0500

    proyecto base

---
+++ src/main/java/com/manga/ahegao/AhegaoApplication.java
@@ -0,0 +1,13 @@
+package com.manga.ahegao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhegaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhegaoApplication.class, args);
+	}
+
+}
